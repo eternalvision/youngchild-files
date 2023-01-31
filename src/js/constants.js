bee3a44b91cd25b38546c8377872b478cc82f9e3
Sasha Priadchenko <56647239+eternalvision@users.noreply.github.com>
2023-01-31 10:19:44 +0100

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,5 @@
 //! API
-export const API_KEY = '22714133-87c7cc427dfc460d08f350db1';
+export const API_KEY = '';
 export const API_URL = 'https://pixabay.com/api/';
 export let HREF = document.URL.split('?');
 if (HREF[1]) {
